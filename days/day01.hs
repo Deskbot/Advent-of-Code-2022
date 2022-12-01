e5e6f2f5bbf4744247b090b5d07fd37e51d0b63f
Thomas Richards <Deskbot@users.noreply.github.com>
2022-12-01 22:09:15 +0000

    part2 attempt

--- days/day01.hs
+++ days/day01.hs
@@ -3,19 +3,37 @@ import Data.Functor
 import Data.List.Split
 import Debug.Trace
 
-main = readFile "inputs/day01.txt"
-    <&> part1
-    <&> show
-    >>= putStrLn
+main = do
+    file <- readFile "inputs/day01.txt"
+    () <- putStrLn $ show $ part1 file
+    putStrLn $ show $ part2 file
 
+makeElvesFromInput :: String -> [[Int]]
+makeElvesFromInput input = splitOn "\n\n" input
+    <&> splitOn "\n"
+    <&> noEmptyCalories
+    <&> fmap (\calorieStr -> read calorieStr :: Int)
 
-part1 file = foldl1 max elves
     where
-    elves :: [Integer]
-    elves = splitOn "\n\n" file
-        <&> splitOn "\n"
-        <&> noEmptyCalories
-        <&> fmap (\calorieStr -> read calorieStr :: Integer)
-        <&> sum
-
     noEmptyCalories = filter (\calories -> calories /= "")
+
+part1 file = foldl1 max elfTotCalories
+    where
+        elfTotCalories = makeElvesFromInput file <&> sum
+
+part2 file = sum bestElves
+    where
+        bestElves = maxn elfTotCalories 3
+        elfTotCalories = makeElvesFromInput file <&> sum
+
+maxn :: [Int] -> Int -> [Int]
+maxn [] _ = []
+maxn arr count = if length arr <= count
+    then traceShowId arr
+    else if head arr > foldl1 min maxnOfRemaining
+        then (head arr):(maxn remaining (count-1))
+        else maxnOfRemaining
+
+    where
+        remaining = tail arr
+        maxnOfRemaining = maxn remaining count
